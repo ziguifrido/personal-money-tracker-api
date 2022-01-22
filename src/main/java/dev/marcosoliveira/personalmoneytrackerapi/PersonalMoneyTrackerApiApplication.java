57aee312f4566c9cecbaf676bbc188dbd9870f9b
Marcos Oliveira <ziguifrido@gmail.com>
2022-01-22 00:40:24 -0400

    Initial Commit

---
+++ src/main/java/dev/marcosoliveira/personalmoneytrackerapi/PersonalMoneyTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package dev.marcosoliveira.personalmoneytrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalMoneyTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalMoneyTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marcosoliveira/personalmoneytrackerapi/PersonalMoneyTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcosoliveira.personalmoneytrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalMoneyTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
