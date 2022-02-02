2aa7960ddd815cdbe9656db88928f8be31efe930
Marcos Oliveira <ziguifrido@gmail.com>
2022-02-01 21:36:57 -0400

    Creating Income GET (by id) endpoint

--- src/main/java/dev/marcosoliveira/personalmoneytrackerapi/controller/IncomeController.java
+++ src/main/java/dev/marcosoliveira/personalmoneytrackerapi/controller/IncomeController.java
@@ -1,9 +1,12 @@
 package dev.marcosoliveira.personalmoneytrackerapi.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +27,16 @@ public class IncomeController {
     List<Income> incomeList = incomeRepository.findAll();
 
     return IncomeDto.convert(incomeList);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<IncomeDto> detail(@PathVariable Long id){
+    Optional<Income> optional = incomeRepository.findById(id);
+
+    if (!optional.isPresent())
+      return ResponseEntity.notFound().build();
+
+    return ResponseEntity.ok(new IncomeDto(optional.get()));
   }
   
 }
